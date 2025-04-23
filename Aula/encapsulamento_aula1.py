e255c27e6372a31e29655ccd357a3ee11bb3633b
Carol <caroldazevedo52@gmail.com>
2025-04-23 14:06:07 -0300

    Update encapsulamento_aula1.py

--- Aula/encapsulamento_aula1.py
+++ Aula/encapsulamento_aula1.py
@@ -23,11 +23,15 @@ return math.PI * self.r ** 2
 
 class UI:
  @staticmethod
+ def menu():
+   print( "triangulo = 1, retangulo = 2, circulo = 3, fim = 9")
+   return int(input())
  def main():
-   x = triangulo()
-   x.b = 10
-   x.h = 20
-   print(x.b, x.h, x.cal_area())
-   print(x)
+ #def main():
+   #x = triangulo()
+   #x.b = 10
+   #x.h = 20
+   #print(x.b, x.h, x.cal_area())
+   #print(x)
 
 UI.main()
