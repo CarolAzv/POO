196c9b28c941d9282034a55aa86a503d9718fa82
Carol <caroldazevedo52@gmail.com>
2025-04-28 17:41:43 -0300

    Create aula_????.cpp

---
+++ Aula/aula_????.cpp
@@ -0,0 +1,12 @@
+int main() {
+  int x = 0; 
+  int y = 5;
+  int z;
+
+  int *p = &y;
+  p = p + 1
+
+  count << x << " " << &x << endl;
+  count << y << " " << &x << endl;
+  count << z << " " << &x << endl;
+}
