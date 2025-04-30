fdfd4ec66dd302847fd4cbfdf10195277187841c
Carol <caroldazevedo52@gmail.com>
2025-04-30 14:16:26 -0300

    Update list3_q1.py

--- Lista3/list3_q1.py
+++ Lista3/list3_q1.py
@@ -1,1 +1,13 @@
+Class Retangulo:
+    def retangulo
+        self.__b = 0
+        self.__h = 0
 
+class UI:
+    @staticmethod
+    def main():
+        x = Triangulo(10, 20)
+        print(x.calc_area())
+        x.set_base(30)
+        x.set_altura(40)
+        print(x.calc_area())
