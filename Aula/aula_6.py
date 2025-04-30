22e7f7b2024029c50cb981f7a604dcb8290fef10
Carol <caroldazevedo52@gmail.com>
2025-04-30 13:37:14 -0300

    Create aula_6.py

---
+++ Aula/aula_6.py
@@ -0,0 +1,16 @@
+class Triangulo:
+  def __init__(self, b, h):
+    self.__b = b
+    self.__h = h
+  def set_base(self, v):
+    if v >=0: self.__b = v
+    else: raise ValueError()
+  def set_altura(self, v):
+    if v >=0: self.__h = v
+    else: raise ValueError()
+  def get_base(self):
+    return self.__b
+  def get_altura(self):
+    return self.__h
+  def calc_area(self):
+    return self.__b * self.__h /2
