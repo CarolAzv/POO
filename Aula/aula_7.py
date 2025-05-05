910bd593a88c5e6393b8893349f2c21352d7579d
Carol <caroldazevedo52@gmail.com>
2025-05-05 17:35:19 -0300

    Create aula_7.py

---
+++ Aula/aula_7.py
@@ -0,0 +1,20 @@
+x = []
+y = list()
+
+
+print(z)
+print(*z)
+
+for elem in z:
+  print(elem)
+
+l = z
+l.append("Tads")
+print(z)
+print(id(l),(id(z))
+
+l2 = z[:]
+l2.append("POO")
+
+print(z)
+print(l2)
