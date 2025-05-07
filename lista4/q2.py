fa75aa6df98199f491b0e2dfa35d7331ebaa257f
Carol <caroldazevedo52@gmail.com>
2025-05-07 13:23:34 -0300

    Update q2.py

--- lista4/q2.py
+++ lista4/q2.py
@@ -16,7 +16,46 @@ class Musica:
     self.__artista = artista
     self.__album = album
   def__str__(self):
-    return f"(self.__titulo) - (self.__artista) - (self.__album)
+    return f"(self.__titulo) - (self.__artista) - (self.__album)"
+    
+class UI:
+  @statecmethod
+  def menu():
+    print("Menu: 1-Criar PlayList, 2-Inserir Musica. 3- listar Musica, 9-Fim"
+
+  @statecmethod
+  def mein():
+    op = 0
+    x = None
+    while op != 9:
+      if op = 1: x = UI.criar_playlist()
+      if op = 2: UI.inserir_musica(x)
+      if op = 3: UI.listar_musica(x)
+
+  @statecmethod
+  def criar_playlist():
+    nome = input("informe o nome da playlist")
+    descrição = input("informe uma descrição")
+    x = PlayList(nome, descrição)
+    return x
+    
+  @statecmethod
+  def inserir_musica(x):
+    titulo = input("informe o titulo da musica: ")
+    artista = input("informe o artista/banda: ")
+    album - input("informe o álbum: ")
+    musica = Musica(titulo, artista, album)
+    x.inserir(musica)
+
+  @statecmethod
+  def listar_musica(x):
+    print(x)
+    for musica in x.listar()
+      print(" ", musica)
+
+
+UI.main()
+    
 
 x = PlayList("Rock", "Minhas músicas de rock preferidas")
 y = Playlist("Axê", "show de Ivete Sangalo - Maracanã")
