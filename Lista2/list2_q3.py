2b98ffd1eb68a98f66f0843cc5e14cf92cdddb02
Carol <caroldazevedo52@gmail.com>
2025-05-21 13:15:53 -0300

    Add files via upload

---
+++ Lista2/list2_q3.py
@@ -0,0 +1,6 @@
+nota1 = int(input("Digite a nota do primeiro bimestre da disciplina: "))
+nota2 = int(input("Digite a nota do segundo bimestre da disciplina: "))
+
+nota = (nota1 * 2) + (nota2 * 3)
+
+print("Média parcial = {}".format(nota/5))
