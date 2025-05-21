1522df14e8eeb94740a331c915966678743f4922
Carol <caroldazevedo52@gmail.com>
2025-05-21 13:15:37 -0300

    Delete Lista2/list2_q4.py

--- Lista2/list2_q4.py
+++
@@ -1,8 +0,0 @@
-base = int(input("Digite a base e a altura do retângulo: "))
-altura = int(input())
-
-area = base * altura
-peri = (base*2) + (altura*2)
-dia = peri ** 0.5
-
-print("Área = {} - Perímetro = {} - Diagonal = {}".format(area, peri, dia))
