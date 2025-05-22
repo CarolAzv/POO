76d4415069e8a84d47101950bfdb0000af7b4dc4
Carol <caroldazevedo52@gmail.com>
2025-05-22 13:43:57 -0300

    Update list3_q2.py

--- Lista3/list3_q2.py
+++ Lista3/list3_q2.py
@@ -1,18 +1,19 @@
 class Frete:
     def __init__(self, dist, peso):
-        self.__dist = dist
-        self.__peso = peso
+        self.set_dist(dist)
+        self.set_peso(peso)
         self.__frete = 0
 
     def set_dist(self, dist):
+        if dist < 0: raise ValueError("Distância não pode ser negativo")
         self.__dist = dist
     def get_dist(Self):
         return self.__dist
 
     def set_peso(self, peso):
+        if peso < 0: raise ValueError("Peso não pode ser negativo")
         self.__peso = peso
     def get_peso(Self):
         return self.__peso
 
     def calc_frete(self):
-        
