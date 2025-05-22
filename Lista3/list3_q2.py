22672c325312bc186896abcdfcd775aff86b03c0
Carol <caroldazevedo52@gmail.com>
2025-05-22 13:36:03 -0300

    Update list3_q2.py

--- Lista3/list3_q2.py
+++ Lista3/list3_q2.py
@@ -1,2 +1,18 @@
 class Frete:
-    
+    def __init__(self, dist, peso):
+        self.__dist = dist
+        self.__peso = peso
+        self.__frete = 0
+
+    def set_dist(self, dist):
+        self.__dist = dist
+    def get_dist(Self):
+        return self.__dist
+
+    def set_peso(self, peso):
+        self.__peso = peso
+    def get_peso(Self):
+        return self.__peso
+
+    def calc_frete(self):
+        
