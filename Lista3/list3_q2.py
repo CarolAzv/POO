ef52e560963026bf7c31a419054a7e5d5942a079
Carol <caroldazevedo52@gmail.com>
2025-05-26 17:27:41 -0300

    Update and rename list3_q2(notyet).py to list3_q2.py

--- Lista3/list3_q2.py
+++ Lista3/list3_q2.py
@@ -1,8 +1,8 @@
 class Frete:
-    def __init__(self, dist, peso):
-        self.set_dist(dist)
+    def __init__(self, peso, dist):
         self.set_peso(peso)
-        self.__frete = 0
+        self.set_dist(dist)
+        self.__frete = self.calc_frete()
 
     def set_dist(self, dist):
         if dist < 0: raise ValueError("Distância não pode ser negativo")
@@ -17,3 +17,13 @@ class Frete:
         return self.__peso
 
     def calc_frete(self):
+        frete = self.__dist * self.__peso
+        frete = frete * 0.01
+        return frete
+    
+    def __str__(self):
+            return f"O frete de transportar {self.__peso}Kg por {self.__dist}Km sera: R${self.__frete}"
+
+
+x = Frete(60, 150)
+print(x)
