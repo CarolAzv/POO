be00d9b64073a9793abba6d31c0bdd7042b291b7
Carol <caroldazevedo52@gmail.com>
2025-05-29 13:23:51 -0300

    Update list3_q4_p1_notyet.py

--- Lista3/list3_q4_p1_notyet.py
+++ Lista3/list3_q4_p1_notyet.py
@@ -1,11 +1,13 @@
 class Equação2g():
-    def __init__(Self, a, b, c):
+    def __init__(self, a, b, c):
         self.__a = a
         self.__b = b
         self.__c = c
         self.__delta = self.Delta()
         self.__raiz1 = self.Raiz1()
         self.__raiz2 = self.Raiz2()
+        self.__raizreal = []
+        self.TemRaizesReais()
 
     def set_a(self, a):
         self.__a = a
@@ -25,14 +27,23 @@ class Equação2g():
         delta = (self.__b * self.__b) - (4 * self.__a * self.__c)
         return delta
     def Raiz1(self):
-        raiz = -b + (self.__delta * 0.5)
+        raiz = -self.__b + (self.__delta * 0.5)
         raiz = raiz / (2 * self.__a)
         return raiz
     def Raiz2(self):
-        raiz = -b - (self.__delta * 0.5)
+        raiz = -self.__b - (self.__delta * 0.5)
         raiz = raiz / (2 * self.__a)
         return raiz
     def TemRaizesReais(self):
+        if self.__raiz1 >= 0:
+            self.__raizreal.append(self.__raiz1)
+        if self.__raiz2 >= 0:
+            self.__raizreal.append(self.__raiz2)
+        return self.__raizreal
 
     def __str__(self):
-        return f""
+        return f"{self.__raizreal}"
+
+
+x = Equação2g(3, 2, 3)
+print(x)
