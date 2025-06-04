e35dee504bed48bb0c4e30be299799357553241c
Carol <caroldazevedo52@gmail.com>
2025-06-04 13:52:29 -0300

    Update list4_q3.notyet.py

--- Lista4/list4_q3.notyet.py
+++ Lista4/list4_q3.notyet.py
@@ -45,3 +45,53 @@ class Cliente():
     
     def __str__(self):
         return f"{self.__nome}, tem limite de R$ {self.__limite}"
+
+class UI:
+    def menu():
+        return int(input("Menu: 1-Criar Empresa, 2-Listar clientes, 3-definir sociedade, 9-sair"))
+    
+    def main():
+        opt = 0
+        x = None
+        while opt != 9:
+            opt = UI.menu()
+            if opt == 1:
+                x = UI.criar_empresa()
+            if opt == 2:
+                x = UI.listar_clientes
+            if opt == 3:
+            if opt == 9:
+
+
+
+    @staticmethod
+    def criar_empresa():
+        nome = input("Informe o nome da impresa: ")
+        x = Empresa(nome)
+
+    @staticmethod
+    def listar_clientes(x):
+        input(x)
+        for clientes in x.Listar():
+            print(" ", clientes)
+
+    @staticmethod
+    def defenir_sociedade():
+        input(x)
+        n = 0
+        clientes = x.listar()
+        for cliente in x.listar():
+            print(x, "-", cliente)
+            n += 1
+        a = int(input("Informe o n do 1 cliente: ")
+        b = int(input("Informe o n do 2 cliente: ")
+        clientes[a].set_social(cliente[b])
+Ui.main()
+
+x = Empresa("parasocio")
+a = Cliente("Bianca", 155.732156012, 2500, "")
+b = Cliente("Andre", 105.235789343, 5000, "")
+x.Inserir(a)
+x.Inserir(b)
+
+print(x.Listar())
