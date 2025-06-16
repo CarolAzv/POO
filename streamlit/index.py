f891dc4b695563fb48c636d0397ec9eb314d3a9e
Carol <caroldazevedo52@gmail.com>
2025-06-16 17:24:16 -0300

    Create index.py

---
+++ streamlit/index.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+from retanguloUI2 import RetanguloUI2
+
+#RetanguloUI.main()
+RetanguloUI2.main()
+
+#st.header("POO em Python com Streamlit")
+#if st.button("Clique Aqui"):
+#    st.write("Bem-vindo(a) ao Streamlit")
